666631e5870975b49a18a89ce234ae63eff496f3
pearlsilver35 <pearlsilver35@gmail.com>
2020-07-20 13:35:36 +0100

    core details

--- config/core.php
+++ config/core.php
@@ -7,12 +7,12 @@ date_default_timezone_set('Africa/Lagos');
  
 // variables used for jwt
 $key = "iJKV1QiLCJhbGci";
-$iss = "http://Businessloanserver.org";
-$aud = "http://businessloan.com";
+$iss = "http://lpmserver.org";
+$aud = "http://lpmserver.com";
 $iat = time();
 $nbf = $iat;
 $exp = $nbf + 14400;
 
 //Passwordhash Variable
-$pepper = "iOiJKV1QiLCJhbGciOiJIUzI1NiJ9";
+$pepper = "iOiJKV1QijjkllkjjlllLCJhbGciNiJ9";
 ?>
